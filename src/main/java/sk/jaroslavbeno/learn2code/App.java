8294614ec1ec04d8db1589cd37e2689f1a212963
Stefan Levoca <stefanlevoca@gmail.com>
2020-10-25 10:22:38 +0100

    Initial commit

---
+++ src/main/java/sk/jaroslavbeno/learn2code/App.java
@@ -0,0 +1,15 @@
+package sk.jaroslavbeno.learn2code;
+
+import org.apache.commons.lang3.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
